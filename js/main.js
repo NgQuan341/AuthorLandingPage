2b5326e7256905c6517d37d2352f5a9c3bbb142d
NgQuan341 <nghongquan34@gmail.com>
2022-06-10 10:46:48 +0700

    update: add active header item when scroll

--- js/main.js
+++ js/main.js
@@ -57,9 +57,27 @@ const activeMenuChapterScroll = function () {
     }
   });
 };
+const activeMenuHeaderScroll = function (){
+  const sections = document.querySelectorAll(".section");
+  const menu_links = document.querySelectorAll(".header-menu-item-text");
+  const makeActive = (link) => menu_links[link].classList.add("active");
+  const removeActive = (link) => menu_links[link].classList.remove("active");
+  const removeAllActive = () =>[...Array(sections.length).keys()].forEach((link) => removeActive(link));
+  const sectionMargin = 0;
+  let currentActive = 0;
+  window.addEventListener("scroll", () => {
+    const current = sections.length - [...sections].reverse().findIndex((section) => window.scrollY >= section.offsetTop + sectionMargin) - 1;
+    if (current < sections.length && current !== currentActive) {
+      removeAllActive();
+      currentActive = current;
+      makeActive(current);
+    }
+  });
+}
 document.addEventListener("DOMContentLoaded",function () {
-    fixedHeader()
+    fixedHeader();
     activeMenuHeader();
     activeMenuChapterScroll();
+    activeMenuHeaderScroll();
   },false
 );
