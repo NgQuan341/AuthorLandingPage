8b6a42cf535060469f591cc354cc30d6905e0ffa
NgQuan341 <nghongquan34@gmail.com>
2022-06-15 15:48:40 +0700

    create: responsive header

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-const fixedHeader = function(){
+const fixedHeader = function () {
   window.addEventListener("scroll", function () {
-    if (window.scrollY >= 100) {
+    if (window.scrollY >= 10) {
       document.getElementById("header").classList.add("fixed");
     } else {
       document.getElementById("header").classList.remove("fixed");
     }
   });
-}
+};
 const activeMenuHeader = function () {
   const menuItems = document.getElementsByClassName("header-menu-item");
   const checkClassActive = function (element) {
@@ -45,11 +45,19 @@ const activeMenuChapterScroll = function () {
   const menu_links = document.querySelectorAll(".chapter-menu ul li a");
   const makeCurrent = (link) => menu_links[link].classList.add("current");
   const removeCurrent = (link) => menu_links[link].classList.remove("current");
-  const removeAllCurrent = () =>[...Array(sections.length).keys()].forEach((link) => removeCurrent(link));
+  const removeAllCurrent = () =>
+    [...Array(sections.length).keys()].forEach((link) => removeCurrent(link));
   const sectionMargin = 1500;
   let currentActive = 0;
   window.addEventListener("scroll", () => {
-    const current = sections.length - [...sections].reverse().findIndex((section) => window.scrollY >= section.offsetTop + sectionMargin) - 1;
+    const current =
+      sections.length -
+      [...sections]
+        .reverse()
+        .findIndex(
+          (section) => window.scrollY >= section.offsetTop + sectionMargin
+        ) -
+      1;
     if (current < sections.length && current !== currentActive) {
       removeAllCurrent();
       currentActive = current;
@@ -57,27 +65,57 @@ const activeMenuChapterScroll = function () {
     }
   });
 };
-const activeMenuHeaderScroll = function (){
+const activeMenuHeaderScroll = function () {
   const sections = document.querySelectorAll(".section");
   const menu_links = document.querySelectorAll(".header-menu-item-text");
   const makeActive = (link) => menu_links[link].classList.add("active");
   const removeActive = (link) => menu_links[link].classList.remove("active");
-  const removeAllActive = () =>[...Array(sections.length).keys()].forEach((link) => removeActive(link));
+  const removeAllActive = () =>
+    [...Array(sections.length).keys()].forEach((link) => removeActive(link));
   const sectionMargin = 0;
   let currentActive = 0;
   window.addEventListener("scroll", () => {
-    const current = sections.length - [...sections].reverse().findIndex((section) => window.scrollY >= section.offsetTop + sectionMargin) - 1;
+    const current =
+      sections.length -
+      [...sections]
+        .reverse()
+        .findIndex((section) => window.scrollY >= section.offsetTop - 112) -
+      1;
     if (current < sections.length && current !== currentActive) {
       removeAllActive();
       currentActive = current;
       makeActive(current);
     }
   });
-}
-document.addEventListener("DOMContentLoaded",function () {
+};
+const activeSlideTestimonial = function () {
+  const blocks = document.querySelectorAll(".testimonial-slide-block");
+  let transition = ` `;
+  [...blocks].forEach((block) => {
+    block.setAttribute("style", transition);
+  });
+};
+const clickMenuButton = function () {
+  let btn = document.querySelector(".menu-btn");
+  let menu = document.querySelector(".header-menu");
+  let click = false;
+  btn.addEventListener("click", function () {
+    if (menu.style.maxHeight) {
+        menu.style.maxHeight = null;
+      } else {
+        menu.style.maxHeight = menu.scrollHeight + "px";
+      }
+  });
+};
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
     fixedHeader();
     activeMenuHeader();
     activeMenuChapterScroll();
     activeMenuHeaderScroll();
-  },false
+    setInterval(activeSlideTestimonial(), 0);
+    clickMenuButton();
+  },
+  false
 );
