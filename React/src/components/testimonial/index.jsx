5ee1be03d3fca9a653417f7cc797e46e18055726
NgQuan341 <nghongquan34@gmail.com>
2022-06-28 22:55:14 +0700

    update: slider testimonial

--- React/src/components/testimonial/index.jsx
+++ React/src/components/testimonial/index.jsx
@@ -1,81 +1,93 @@
-import React, { useState } from 'react'
-import SlideItem from './SlideItem'
-import style from './testimonial.module.css'
+import React, { useState } from "react";
+import SlideItem from "./SlideItem";
+import style from "./testimonial.module.css";
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
+import '@splidejs/react-splide/css';
 
 const Testimonial = () => {
   const [slides, setSlides] = useState([
     {
-      id:1,
-      img:`./assets/images/person1.webp`,
-      name:`Roger Scott`,
-      position:`Marketing Manager`,
-      description:`Far far away, behind the word mountains, far from the countries Vokalia
-      and Consonantia, there live the blind texts.`
+      id: 1,
+      img: `./assets/images/person1.webp`,
+      name: `Roger Scott`,
+      position: `Marketing Manager`,
+      description: `Far far away, behind the word mountains, far from the countries Vokalia
+      and Consonantia, there live the blind texts.`,
     },
     {
-      id:2,
-      img:`./assets/images/person2.webp`,
-      name:`Roger Scott`,
-      position:`Marketing Manager`,
-      description:`Far far away, behind the word mountains, far from the countries Vokalia
-      and Consonantia, there live the blind texts.`
+      id: 2,
+      img: `./assets/images/person2.webp`,
+      name: `Roger Scott`,
+      position: `Marketing Manager`,
+      description: `Far far away, behind the word mountains, far from the countries Vokalia
+      and Consonantia, there live the blind texts.`,
     },
     {
-      id:3,
-      img:`./assets/images/person3.webp`,
-      name:`Roger Scott`,
-      position:`Marketing Manager`,
-      description:`Far far away, behind the word mountains, far from the countries Vokalia
-      and Consonantia, there live the blind texts.`
+      id: 3,
+      img: `./assets/images/person3.webp`,
+      name: `Roger Scott`,
+      position: `Marketing Manager`,
+      description: `Far far away, behind the word mountains, far from the countries Vokalia
+      and Consonantia, there live the blind texts.`,
     },
     {
-      id:4,
-      img:`./assets/images/person1.webp`,
-      name:`Roger Scott`,
-      position:`Marketing Manager`,
-      description:`Far far away, behind the word mountains, far from the countries Vokalia
-      and Consonantia, there live the blind texts.`
+      id: 4,
+      img: `./assets/images/person1.webp`,
+      name: `Roger Scott`,
+      position: `Marketing Manager`,
+      description: `Far far away, behind the word mountains, far from the countries Vokalia
+      and Consonantia, there live the blind texts.`,
     },
-  ])
+  ]);
   return (
     <>
-    <section className={`${style.testimonial} section`} id="reviews">
-        <div className={style.testimonial_img} style={{backgroundImage: `url(./assets/images/testimonial.webp)`}}></div>
+      <section className={`${style.testimonial} section`} id="reviews">
+        <div
+          className={style.testimonial_img}
+          style={{ backgroundImage: `url(./assets/images/testimonial.webp)` }}
+        ></div>
         <div className={style.testimonial_container}>
           <div className={style.testimonial_text}>
             <p>Testimonial</p>
             <h2>Kinds Words From Customers</h2>
           </div>
-          <div className={style.testimonial_slide_container}>
-            <div className={style.testimonial_slide_wrapper}>
-              <div className={style.testimonial_slide_main}>
-               {
-                 slides.map(slide=>(
-                   <SlideItem key={slide.id} slide={slide} style={style} />
-                 ))
-               }
-              </div>
-            </div>
-          </div>
-          <div className={style.testimonial_dots}>
-            <button className={`${style.btn} ${style.btn_next}`}>
-              <i className="fa-solid fa-angle-left"></i>
-            </button>
-            <div className={style.dots}>
-            {
-              slides.map(slide=>(
-                <button key={slide.id}></button>
-              ))
-            }
-            </div>
-            <button className={`${style.btn} ${style.btn_pre}`}>
-              <i className="fa-solid fa-angle-right"></i>
-            </button>
-          </div>
+            <Splide aria-label="Testimonial" options={{
+              type:'loop',
+              perPage: 3,
+              perMove: 1,
+              gap:`15px`,
+              autoplay: true,
+              arrows: false,
+              speed: 1500,
+              pauseOnHover: true,
+              breakpoints: {
+                992: {
+                  perPage: 1,
+                  padding:`172px`
+                },
+                768:{
+                  perPage: 1,           
+                },
+                590:{
+                  perPage: 1,
+                  gap:`30px`,
+                  padding:{right:'29px'}
+                }
+              }
+            }}>
+              {
+                slides.map((slide) => (
+                  <SplideSlide key={slide.id}>
+                    <SlideItem slide={slide} style={style} />
+                  </SplideSlide>
+                ))
+              }
+              
+            </Splide>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Testimonial
+export default Testimonial;
